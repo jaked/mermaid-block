4b87064c9aa92606c9795e502ccf0f211dd0fa46
Jake Donham <jaked@github.com>
2022-02-14 15:25:43 -0800

    Initial commit

---
+++ src/blocks/example-file-block/index.tsx
@@ -0,0 +1,35 @@
+import {
+  FileBlockProps,
+  getLanguageFromFilename,
+} from "@githubnext/utils";
+import "./index.css";
+
+export default function (props: FileBlockProps) {
+  const { context, content, metadata, onUpdateMetadata } = props;
+  const language = Boolean(context.file)
+    ? getLanguageFromFilename(context.file)
+    : "N/A";
+
+
+  return (
+    <div className="Box m-4">
+      <div className="Box-header">
+        <h3 className="Box-title">
+          File: {context.path} {language}
+        </h3>
+      </div>
+      <div className="Box-body">
+        Metadata example: this button has been clicked{" "}
+        <button
+          className="btn"
+          onClick={() =>
+            onUpdateMetadata({ number: (metadata.number || 0) + 1 })
+          }
+        >
+          {metadata.number || 0} times
+        </button>
+        <pre className="mt-3 p-3">{content}</pre>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/local-block.tsx
@@ -0,0 +1,125 @@
+import { useCallback, useEffect, useState } from "react";
+// @ts-ignore
+import loadable from "@loadable/component";
+import { FileContext, FolderContext, RepoFiles } from "@githubnext/utils";
+import { getFileContent, getRepoInfo } from "../hooks";
+
+interface Block {
+  id: string;
+  type: string;
+  title: string;
+  description: string;
+  entry: string;
+  extensions?: string[];
+  owner?: string;
+  repo?: string;
+}
+interface LocalBlockProps {
+  block: Block;
+  contents?: string;
+  tree?: RepoFiles;
+  metadata?: any;
+  context: FileContext | FolderContext
+}
+export const LocalBlock = (props: LocalBlockProps) => {
+  const {
+    block,
+    contents,
+    tree,
+    metadata = {},
+    context,
+  } = props;
+
+  const [Block, setBlock] = useState<React.ComponentType<any> | null>(null);
+
+  const getContents = async () => {
+    const content = await loadable(() => import(
+      `../../..${block.entry}`
+    ))
+    setBlock(content)
+  }
+  useEffect(() => { getContents() }, [block.entry])
+
+  const onUpdateMetadata = (newMetadata: any) => {
+    console.log(`Triggered a request to update the file contents`)
+    console.log("From:", metadata)
+    console.log("To:", newMetadata)
+  }
+  const onNavigateToPath = useCallback((path) => {
+    console.log(`Triggered a navigation to the file/folder: ${path}`)
+  }, [])
+  const onRequestUpdateContent = useCallback((content) => {
+    console.log(`Triggered a request to update the file contents`)
+    console.log("From:", contents)
+    console.log("To:", content)
+  }, [])
+  const onRequestGitHubData = async (type: string, config: FileContext | FolderContext, id: string) => {
+    const data = await fetchGitHubData(type, config);
+    window.postMessage({
+      type: "github-data--response",
+      id,
+      data,
+    }, "*");
+    return data
+  }
+
+  if (!Block) return null
+  return (
+    <Block
+      context={context}
+      content={contents}
+      tree={tree}
+      metadata={metadata}
+      onUpdateMetadata={onUpdateMetadata}
+      onNavigateToPath={onNavigateToPath}
+      onRequestUpdateContent={onRequestUpdateContent}
+      onRequestGitHubData={onRequestGitHubData}
+    />
+  )
+}
+
+
+const getBlockKey = (block: Block) => (
+  `${block?.owner}/${block?.repo}__${block?.id}`.replace(
+    /\//g,
+    "__"
+  )
+)
+const getMetadataPath = (block: Block, path: string) => (
+  `.github/blocks/${block?.type}/${getBlockKey(block)}/${encodeURIComponent(path)}.json`
+)
+const fetchGitHubData = async (type: string, config: FileContext | FolderContext) => {
+  if (type === "file-content") {
+    const data = await getFileContent({
+      owner: config.owner,
+      repo: config.repo,
+      path: config.path,
+      fileRef: config.fileRef || "HEAD",
+    })
+    return data;
+  } else if (type === "metadata") {
+    try {
+      const res = await getFileContent({
+        owner: config.owner,
+        repo: config.repo,
+        path: getMetadataPath(config.block, config.path),
+        fileRef: "HEAD",
+        cache: new Date().toString(),
+      })
+      const fullMetadata = JSON.parse((res.content || "{}") as string);
+      return fullMetadata || {}
+    } catch (e) {
+      return {}
+    }
+  } else if (type === "repo-info") {
+    try {
+      const res = await getRepoInfo({
+        owner: config.owner,
+        repo: config.repo,
+      })
+      return res
+    } catch (e) {
+      return {}
+    }
+  }
+}
